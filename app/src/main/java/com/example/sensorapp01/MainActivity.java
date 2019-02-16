e279e05bad29d905175f2912ad4046aba29ef5de
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-02-16 16:31:11 +0900

    センサーリストを表示する。

--- app/src/main/java/com/example/sensorapp01/MainActivity.java
+++ app/src/main/java/com/example/sensorapp01/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sensorapp01;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorManager sm = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor s: sensors) {
+            Log.d("SENSOR", "name: " + s.getName() + " type:" + String.valueOf(s.getType()));
+        }
     }
 }
