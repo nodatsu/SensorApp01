867cd5598b0ee6a592cfa9cb8e0f815642ad5046
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-02-16 16:52:01 +0900

    加速度センサーの値を表示する。

--- app/src/main/java/com/example/sensorapp01/MainActivity.java
+++ app/src/main/java/com/example/sensorapp01/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sensorapp01;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +11,7 @@ import android.util.Log;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         for (Sensor s: sensors) {
             Log.d("SENSOR", "name: " + s.getName() + " type:" + String.valueOf(s.getType()));
         }
+
+        Sensor accSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sev) {
+        float[] val = sev.values;
+        Log.d("SENSOR", "x:" + val[0] + " y:" + val[1] + " z:" + val[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor s, int ac) {
+
     }
 }
